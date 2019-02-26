efbb5b92496ff7c17f32c9cb113886b85dd54417
bangnthree <48021500+bangnthree@users.noreply.github.com>
2019-02-26 22:39:11 +0800

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,120 @@
+<?php
+/* setting bot */
+
+$ads=$pth."[support:".$tr." khairil".$pth."]".$t.$ijo."Remaked By Mohamad Safrin Nangili ".$t.
+$kn."https://safrinnangili.online".$t;
+
+$donasi=false;
+
+$warn=$red."[!] Peringatan !
+".$kn."[-] Ini adalah program ilegal
+".$kn."[-] Resiko sepenuhnya di tanggung pengguna".$t;
+
+
+$baca_plus=$red."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putih
+chanel :$kuning sungging$putih || code invite:$ijo F3NW34owm4".$t;
+$msg_baca= "[•] Remaked By Mohamad Safrin Nangili".$t."[•] Last Update 14/02/2019";
+$stat_baca=true;
+ 
+$ccu= $red."
+──── ▄▀ ▄▀▄ ▀ █▄─█──── ▄▀ █── █─█ █▀▄────".$pth."
+──── █─ █─█ █ █─▀█──── █─ █─▄ █─█ █▀█────
+──── ─▀ ─▀─ ▀ ▀──▀──── ─▀ ▀▀▀ ─▀─ ▀▀─────
+creator: ".$ijo."adidoank ".$pth."|| chanel: ".$ijo."adi bordir".$t;
+$msg_ccu=$ijo."[•] Remaked By Mohamad Safrin Nangili dan sungging";
+$stat_ccu=true;
+
+$mc=$br."
+──── █▄─▄█ ▄▀▄ █▄─█ █▀▀ ▀▄─▄▀──── ▄▀ █─█ █▀▄ █▀▀────
+──── █─█─█ █─█ █─▀█ █▀▀ ──█────── █─ █─█ █▀█ █▀▀────
+──── ▀───▀ ─▀─ ▀──▀ ▀▀▀ ──▀────── ─▀ ─▀─ ▀▀─ ▀▀▀────
+".$pth."creator: ".$ijo."adidoank ".$pth."|| ".$ijo."subcribe chanel adi bordir".$t.$pth."invite : ".$ijo."YCUSP ".$pth."|| ".$ijo."W9HVQ
+".$pth."support: ".$ijo."subcribe chanel sungging".$t;
+$msg_mc=$ijo."[•] This script doesn't work".$t."[•] Script Coid";
+$stat_mc=true;
+
+$flash="$red
+ █▀──── █────── ▄▀▄──── ▄▀▀──── █────── ▄▀▀───── ▄▀▄
+ █▀──── █─▄──── █▀█──── ─▀▄──── █▀▄──── █─▀▌──── █─█
+ ▀───── ▀▀▀──── ▀─▀──── ▀▀───── ▀─▀──── ▀▀▀───── ─▀─
+$putih  creator :$ijo adidoank$putih ||||| chanel :$turkis adi bordir".$t."$putih  support :$ijo sungging$putih ||||| chanel :$turkis sungging";
+$msg_flash=$turkis."[•] Flash GO";
+$stat_flash=true;
+
+$yogov= $kuning."
+ ▀▄─▄▀ ▄▀▄ ▄▀▀─ ▄▀▄──── ▐▌─▐▌ ▀ █▀▄ █▀▀ ▄▀▄
+ ──█── █─█ █─▀▌ █─█──── ─▀▄▀─ █ █─█ █▀▀ █─█
+ ──▀── ─▀─ ▀▀▀─ ─▀───── ──▀── ▀ ▀▀─ ▀▀▀ ─▀─".$t.
+$ijo."[ v 1.0 ] ".$putih."creator: ".$ijo."adidoank".$t.">> subcribe chanel adi bordir".$t.">> subcribe chanel sungging".$t;
+$msg_yogo=$ijo."[•] enjoy your life";
+$stat_yogo=true;
+$yogo=["tes"];
+
+$wot=$pth."
+    ___ Wheel __       __O_f__        _Trust__".$red."
+    __ |     / / ".$kn."      __  __ \ ".$ijo."      ___  __/".$red."
+    __ | /| / /  ".$kn."      _  / / / ".$ijo."      __  /   ".$red."
+    __ |/ |/ /  ".$kn."       / /_/ /  ".$ijo."      _  /    ".$red."
+    ____/|__/   ".$kn."       \____/   ".$ijo."      /_/     ".$pth."
+========[ v 1.0 ][creator: adidoank]========".$t;
+$stat_wot=false;
+$msg_wot=$ijo."[•] skrip dalam perbaikan";
+
+
+$emon=$ijo."
+──── █▀▀──── █▄─▄█──── ▄▀▄──── █▄─█────
+──── █▀▀──── █─█─█──── █─█──── █─▀█────
+──── ▀▀▀──── ▀───▀──── ─▀───── ▀──▀────".$pth."
+==[creator:".$ijo." adidoank".$pth."]=|=".$tr."[ v : 1 . 0 ]".$pth."==".$t;
+$stat_emon=true;
+$auto_ref=true;
+$msg_emon= $ijo."[•] enjoy your life";
+
+$you_me=$red."
+──── ▀▄─▄▀ ▄▀▄ █─█──── █▄─▄█ █▀▀────
+──── ──█── █─█ █─█──── █─█─█ █▀▀────
+──── ──▀── ─▀─ ─▀───── ▀───▀ ▀▀▀────".$t.$pth."========".$ijo."[Remaked : Mohamad Safrin Nangili]".$pth."========".$t;
+$youme=true;
+$msg_you=$ijo."[•] enjoy your life";
+
+$ltc=$red."
+_______________________________________
+    _| [".$ijo."ROOT".$red."]  _|  ".$ijo." |||||||||||||||||||".$red."                   
+    _|       _|_|_|_|       _|_|_|      
+    _|         _|         _|            
+    _|  __     _|         _|         
+_____|____|__".$ijo."I".$red."__|_|___".$ijo."E".$red."____|_|_|_|_".$ijo."OIN_".$pth."
+[ v.1.0 ]========[ Remaked : Mohamad Safrin Nangili ] ".$t;
+$msg_ltc="[•] enjoy your life";
+$do_ltc=false;
+$stat_ltc=true;
+
+$eth_far=$tr."
+──── █▀▀ ▀█▀ █────── █▀ ▄▀▄ █▀▀▄ █▄─▄█ █▀▀ █▀▀▄────
+──── █▀▀ ─█─ █▀▄──── █▀ █▀█ █▐█▀ █─█─█ █▀▀ █▐█▀────
+──── ▀▀▀ ─▀─ ▀─▀──── ▀─ ▀─▀ ▀─▀▀ ▀───▀ ▀▀▀ ▀─▀▀────
+======[".$ijo." version 1.0".$tr." ]===[ creator:".$ijo." adidoank".$tr." ]======".$t;
+$msg_eth_far=$ijo."[•] enjoy your life";
+$stat_eth_far=true;
+
+$ice=$br."
+──── ▀ ▄▀ █▀▀──── █▀▄ █▀▀▄ █▀▀ ▄▀▄ █─▄▀ █▀▀ █▀▀▄────
+──── █ █─ █▀▀──── █▀█ █▐█▀ █▀▀ █▀█ █▀▄─ █▀▀ █▐█▀────
+──── ▀ ─▀ ▀▀▀──── ▀▀─ ▀─▀▀ ▀▀▀ ▀─▀ ▀─▀▀ ▀▀▀ ▀─▀▀────".$pth."
+|||=============[".$ijo." creator: adioank".$pth." ]=============|||".$t;
+$msg_ice=$ijo."[•] update skrip https://github.com/adidoank/break";
+$stat_ice=true;
+
+$epi=$kn."
+ █▀▀──── █▀▄──── ▀──── ▄▀──── ▄▀▄──── ▄▀▀──── █──
+ █▀▀──── █─█──── █──── █───── █▀█──── ─▀▄──── █▀▄
+ ▀▀▀──── █▀───── ▀──── ─▀──── ▀─▀──── ▀▀───── ▀─▀
+ ".$pth."[ ".$tr."v 1.0".$pth." ]==[ ".$tr."creator:".$ijo." adidoank".$pth." ]==[".$tr." ref: ".$ijo."TVFG9W".$pth." ]".$t;
+$msg_epi=$pth."[support:".$tr." khairil".$pth."][Kode: ".$ijo."ZAVGFX]".$t;
+$stat_epi=true;
+
+?>
